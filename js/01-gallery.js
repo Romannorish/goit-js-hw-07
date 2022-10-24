e9461d04249f1163cd0bf100143922aa830238a3
Roman Denysenko <97247092+Romannorish@users.noreply.github.com>
2022-10-24 23:56:29 +0300

    save hw 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,79 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// 1. Створення і рендер розмітки__________________
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryImg = createGalleryImg(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryImg);
+
+console.log(createGalleryImg(galleryItems));
+
+function createGalleryImg(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+           <a class="gallery__link" href="${original}">
+           <img
+           class="gallery__image"
+           src="${preview}"
+           data-source="${original}"
+           alt="${description}"
+           />
+           </a>
+        </div>
+      `;
+    }).join("");
+};
+
+// Делегування і отримання url_______________
+
+galleryContainer.addEventListener('click', onImageClick);
+
+function onImageClick(event) {
+ const isImg = event.target.classList.contains('gallery__image');
+ event.preventDefault();
+  if(!isImg) {
+    return;
+  }
+
+  const imgEl = document.querySelector('.gallery__link');
+  imgEl.href = event.target.dataset.source;
+
+ // //   Створює екземпляр  basicLightbox_______________
+
+  const instance = basicLightbox.create(`
+    <img class="modal__image" src="assets/images/image.png" width="800" height="600">
+    `);
+
+  instance.show();
+  
+  const modalEl = document.querySelector('.modal__image');
+  modalEl.src = event.target.dataset.source;
+ 
+//   // Закриття модалки по escape__________________________
+
+//   window.addEventListener('keydown', closeModalEsc)
+
+//   function closeModalEsc(event) {
+
+//     if(event.key === "Escape") {
+//       instance.close();
+
+//       window.removeEventListener('keydown', closeModalEsc);
+//     }
+//   }
+// };
 
 
-const galleryRef = document.querySelector('.gallery');
-console.log(galleryRef);
 
-
-galleryRef.addEventListener('click', () => {
-});
+//   const instance = basicLightbox.create(`
+//     <img class="modal__image" src="assets/images/image.png" width="800" height="600">
+//     `, {
+// onShow(){
+// document.addEventListener('keydown', closeModalEsc)
+// },
+// onClose(){
+// document.removeEventListener('keydown', closeModalEsc) 
+// }
+// });
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryImg = createGalleryImg(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryImg);
+
+console.log(createGalleryImg(galleryItems));
+
+function createGalleryImg(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+           <a class="gallery__link" href="${original}">
+           <img
+           class="gallery__image"
+           src="${preview}"
+           data-source="${original}"
+           alt="${description}"
+           />
+           </a>
+        </div>
+      `;
+    }).join("");
+};
+
+
+new SimpleLightbox ('.gallery a', { 
+  nav: true,
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+SimpleLightbox();
